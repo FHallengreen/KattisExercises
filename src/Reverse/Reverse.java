3d7953c28bf8f8819b02ee1b1ed172e76d232751
FHallengreen <fred70p9@stud.kea.dk>
2022-05-29 20:37:39 +0200

    Reverse

--- src/Reverse/Reverse.java
+++ src/Reverse/Reverse.java
@@ -1,7 +1,20 @@
 package Reverse;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Reverse {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int [] ary = new int[n];
+        for (int i = 0; i < n; i++) {
+            ary[i] = scanner.nextInt();
+        }
+        for (int j = ary.length -1; j >= 0; j--) {
+            System.out.println(ary[j]);
+        }
     }
 }
